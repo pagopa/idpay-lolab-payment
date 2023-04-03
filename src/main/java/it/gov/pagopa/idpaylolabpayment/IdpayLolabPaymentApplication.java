9a01d16a6b781a5817febd8831f5ff0e720f6eee
Luca Consalvi <luca.consalvi@pagopa.it>
2023-04-03 15:17:47 +0200

    update gitignore and adding a first basic java skeleton

---
+++ src/main/java/it/gov/pagopa/idpaylolabpayment/IdpayLolabPaymentApplication.java
@@ -0,0 +1,13 @@
+package it.gov.pagopa.idpaylolabpayment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IdpayLolabPaymentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IdpayLolabPaymentApplication.class, args);
+	}
+
+}
---
+++ src/test/java/it/gov/pagopa/idpaylolabpayment/IdpayLolabPaymentApplicationTests.java
@@ -0,0 +1,13 @@
+package it.gov.pagopa.idpaylolabpayment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IdpayLolabPaymentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
