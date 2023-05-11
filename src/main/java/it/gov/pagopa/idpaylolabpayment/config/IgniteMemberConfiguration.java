e4f2f4a1b8c7f2248ca34def3d576af55be9583a
petretiandrea <petretiandrea@gmail.com>
2023-05-11 16:36:56 +0200

    direct write to database

--- src/main/java/it/gov/pagopa/idpaylolabpayment/config/IgniteMemberConfiguration.java
+++ src/main/java/it/gov/pagopa/idpaylolabpayment/config/IgniteMemberConfiguration.java
@@ -9,6 +9,8 @@ import org.apache.ignite.configuration.DataStorageConfiguration;
 import org.apache.ignite.configuration.DeploymentMode;
 import org.apache.ignite.configuration.IgniteConfiguration;
 import org.apache.ignite.spi.communication.tcp.TcpCommunicationSpi;
+import org.apache.ignite.spi.metric.jmx.JmxMetricExporterSpi;
+import org.apache.ignite.spi.metric.log.LogExporterSpi;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -28,33 +30,19 @@ public class IgniteMemberConfiguration {
 
   @Bean
   public IgniteConfiguration memberConfiguration(
-      DataRegionConfiguration defaultDataRegion,
       DataRegionConfiguration simpleCacheDataRegion
   ) {
     final var storageConfiguration = new DataStorageConfiguration();
-    storageConfiguration.setDefaultDataRegionConfiguration(defaultDataRegion);
-    storageConfiguration.setDataRegionConfigurations(
-        simpleCacheDataRegion
-    );
+    storageConfiguration.setDefaultDataRegionConfiguration(simpleCacheDataRegion);
+    storageConfiguration.setSystemRegionMaxSize(50L * 1024 * 1024); // MB
 
     return new IgniteConfiguration()
         .setIgniteInstanceName("ignite-cluster")
         .setDataStorageConfiguration(storageConfiguration)
         .setPeerClassLoadingEnabled(true)
         .setDeploymentMode(DeploymentMode.CONTINUOUS)
+        .setMetricExporterSpi(new JmxMetricExporterSpi())
         .setCommunicationSpi(new TcpCommunicationSpi());
-  }
-
-  @Bean
-  public DataRegionConfiguration defaultDataRegion() {
-    final var region = new DataRegionConfiguration();
-    region.setName("Default_Region");
-    region.setInitialSize(20L * 1024 * 1024); // 20 MB
-    region.setMaxSize(50L * 1024 * 1024); // Max 50 MB
-    region.setPageEvictionMode(DataPageEvictionMode.RANDOM_2_LRU);
-    region.setMetricsEnabled(true);
-    region.setEvictionThreshold(0.8); // 80 %
-    return region;
   }
 
   @Bean
--- src/main/java/it/gov/pagopa/idpaylolabpayment/config/SimpleCacheConfiguration.java
+++ src/main/java/it/gov/pagopa/idpaylolabpayment/config/SimpleCacheConfiguration.java
@@ -10,7 +10,6 @@ import org.apache.ignite.cache.CacheMode;
 import org.apache.ignite.configuration.CacheConfiguration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.DependsOn;
 
 @Configuration
 public class SimpleCacheConfiguration {
@@ -29,20 +28,20 @@ public class SimpleCacheConfiguration {
   }
 
   @Bean
-  @DependsOn("transactionReactiveDao")
+  //@DependsOn("transactionReactiveDao")
   public IgniteCache<String, Transaction> transactionCache(Ignite ignite) {
     final var configuration = new CacheConfiguration<String, Transaction>()
         .setName("transaction-cache")
         .setCacheMode(CacheMode.PARTITIONED)
         .setOnheapCacheEnabled(false)
         .setBackups(1)
-        .setReadThrough(true)
-        .setWriteThrough(true)
+        //.setReadThrough(true)
+        //.setWriteThrough(true)
         //.setWriteBehindEnabled(true)
         //.setWriteBehindFlushFrequency(5000) // every 5 seconds flush data
         //.setWriteBehindFlushSize(1000) // evert 1000 update
-        .setTypes(String.class, Transaction.class)
-        .setCacheStoreFactory(new TransactionMongoCacheAdapterFactory().setDataSourceBean("transactionReactiveDao"))
+        //.setTypes(String.class, Transaction.class)
+        //.setCacheStoreFactory(new TransactionMongoCacheAdapterFactory().setDataSourceBean("transactionReactiveDao"))
         .setDataRegionName(SIMPLE_CACHE_EVICTION_REGION);
 
     return ignite.getOrCreateCache(configuration);
